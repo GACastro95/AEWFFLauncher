7ed6f6b5a51bcc8eb5e2de8cd9931bdbcdb16765
Gabriel Castro <gabriel.alfonso.castro@gmail.com>
2024-07-10 09:40:12 +0100

    Add project files.

---
+++ FolderPathSelector.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.IO;
+
+namespace AEWFF_Launcher
+{
+    public partial class FolderPathSelector : Window
+    {
+        public FolderPathSelector(string initialPath1 = "", string initialPath2 = "")
+        {
+            InitializeComponent();
+
+            Path1TextBox.Text = initialPath1;
+            Path2TextBox.Text = initialPath2;
+            if (Path1TextBox.Text == Path2TextBox.Text)
+            {
+                SamePathCheckBox.IsChecked = true;
+            }
+        }
+
+        private void BrowsePath1_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedPath = OpenFolderDialog();
+            if (selectedPath != null)
+            {
+                Path1TextBox.Text = selectedPath;
+                if (SamePathCheckBox.IsChecked == true)
+                {
+                    Path2TextBox.Text = selectedPath;
+                }
+            }
+        }
+
+        private void BrowsePath2_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedPath = OpenFolderDialog();
+            if (selectedPath != null)
+            {
+                Path2TextBox.Text = selectedPath;
+            }
+        }
+
+        private void Submit_Click(object sender, RoutedEventArgs e)
+        {
+            string path1 = Path1TextBox.Text;
+            string path2 = Path2TextBox.Text;
+
+            if (string.IsNullOrEmpty(path1) || (string.IsNullOrEmpty(path2) && SamePathCheckBox.IsChecked == false))
+            {
+                MessageBox.Show("Please select both folder paths.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            Config config = new Config
+            {
+                VanillaGamePath = path1,
+                ModdedGamePath = path2
+            };
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText("Config.json", jsonString);
+
+            MessageBox.Show($"Paths saved.", "Paths Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+            Close();
+
+        }
+
+        private static void SaveConfig()
+        {
+        }
+
+        private void SamePathCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Path1TextBox != null && Path2TextBox != null && BrowsePath2Button != null)
+            {
+                Path2TextBox.Text = Path1TextBox.Text;
+                Path2TextBox.IsEnabled = false;
+                BrowsePath2Button.IsEnabled = false;
+            }
+        }
+
+        private void SamePathCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            Path2TextBox.IsEnabled = true;
+            BrowsePath2Button.IsEnabled = true;
+            Path2TextBox.Text = string.Empty;
+        }
+
+        private string OpenFolderDialog()
+        {
+            var dialog = new OpenFileDialog
+            {
+                CheckFileExists = false,
+                CheckPathExists = true,
+                FileName = "Folder Selection."
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                return Path.GetDirectoryName(dialog.FileName);
+            }
+            return null;
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Input;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Diagnostics;
+
+namespace AEWFF_Launcher
+{
+    public partial class MainWindow : Window
+    {
+        [DllImport("ModBypass.dll")]
+        private static extern int RunLauncher(char[] filePath);
+
+        private static Config config = new Config
+        {
+            VanillaGamePath = "",
+            ModdedGamePath = ""
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadConfig();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void SetupButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            FolderPathSelector folderPathSelector = new FolderPathSelector(config.VanillaGamePath, config.ModdedGamePath);
+
+            folderPathSelector.ShowDialog();
+            LoadConfig();
+        }
+
+        private static void LoadConfig()
+        {
+            if (!File.Exists("Config.json"))
+            {
+                SaveConfig();
+            }
+
+            string configFile = File.ReadAllText("Config.json");
+            config = JsonSerializer.Deserialize<Config>(configFile) ?? config;
+        }
+
+        private static void SaveConfig()
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText("Config.json", jsonString);
+        }
+        
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void StartVanillaGame(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string pathToExe = config.VanillaGamePath + "\\AEWFightForever.exe";
+
+                Environment.SetEnvironmentVariable("SteamAppId", "1913210");
+                Process.Start(pathToExe);
+
+                Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error starting process: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void StartModdedGame(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string pathToExe = config.ModdedGamePath + "\\AEWFightForever\\Binaries\\Win64\\AEWFightForever-Win64-Shipping.exe";
+
+                int errorCode = RunLauncher(pathToExe.ToCharArray());
+                switch (errorCode)
+                {
+                    case 0:
+                        Close();
+                        break;
+                    case 1:
+                        MessageBox.Show($"Game Not Found (Error Code: {errorCode})", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+                    default:
+                        MessageBox.Show($"Unknown Error (Error Code: {errorCode})", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error starting process: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+
+    public class Config
+    {
+        [JsonPropertyName("VanillaGamePath")]
+        public required string VanillaGamePath { get; set; }
+
+        [JsonPropertyName("ModdedGamePath")]
+        public required string ModdedGamePath { get; set; }
+    }
+}
